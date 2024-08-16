919e2a455e5be8dc331d23e5d926677d2e4e06d5
heyitsharman <harmankaur0779@gmail.com>
2024-08-16 19:49:52 +0530

    uploading project on repo

---
+++ src/content_option.js
@@ -0,0 +1,134 @@
+const logotext = "Home";
+const meta = {
+  title: "Harman Kaur",
+  description: "Hey There i am Harman kaur a future software developer",
+};
+
+const introdata = {
+  title: "I’m  Harman Kaur",
+  animated: {
+    first: "I Like coding",
+    second: "I'm learning DSA",
+    third: "I develop websites",
+  },
+  description:
+    "I love turning ideas into reality through code. Explore my portfolio to see how I craft innovative software solutions, and let's collaborate on bringing your next project to life!",
+  your_img_url:
+    "https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2022/10/funky-astronomy-sites-cool-space-universe-featured.jpg",
+};
+
+const dataabout = {
+  title: "About my self",
+  aboutme:
+    "I'm a software developer with a passion for building engaging web experiences. I thrive on turning ideas into reality through code, and I'm constantly seeking new challenges, like venturing into the world of UI/UX design. My experience extends to freelance work, allowing me to collaborate with diverse clients and bring their visions to life.",
+};
+// const worktimeline = [{
+//         jobtitle: "Designer of week",
+//         where: "YAdfi",
+//         date: "2020",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "Jamalya",
+//         date: "2019",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "ALquds",
+//         date: "2019",
+//     },
+// ];
+
+const skills = [{
+        name: "Python",
+        value: 40,
+    },
+    {
+        name: "Javascript",
+        value: 65,
+    },
+    {
+        name: "c++",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    // {
+    //     name: "",
+    //     value: 85,
+    // },
+    // {
+    //     name: "",
+    //     value: 85,
+    // },
+];
+
+const services = [
+  {
+    title: "Full Stack Web Development",
+    description:
+      "I deliver robust and scalable applications, from concept to completion.  I handle both the front-end (user interface) and back-end (server-side logic) development, ensuring a seamless user experience across all devices. My focus is on building applications that can grow with your needs",
+  },
+  {
+    title: "UI/UX design",
+    description:
+      "I create user-centered interfaces that are both beautiful and functional.  My approach prioritizes user needs and behavior to ensure intuitive and visually captivating experiences.  Through prototyping and iteration, I collaborate with you to achieve optimal usability and aesthetics.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://www.shutterstock.com/shutterstock/photos/2128036841/display_1500/stock-vector--s-retro-vaporwave-aesthetics-digital-screen-user-interface-cute-old-computer-ui-elements-2128036841.jpg",
+    description:
+      "Contributed in Hangman game UI which allows multiple player to play the game. In this game, one player selects a word. Other players have a certain number of guesses to guess the characters in the word. If the players are able to guess the characters in the entire word within certain attempts, they win. Otherwise, they lose.",
+    link: "https://github.com/Kanav4002/Hangman_Game_Project",
+  },
+  {
+    img: "https://img.freepik.com/free-vector/developer-laptop-computer-with-open-robotic-soft-open-automation-architecture-open-source-robotics-soft-free-development-concept_335657-1900.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1722643200&semt=ais_hybrid",
+    description: "Modern looking Responsive login page ",
+    link: "https://github.com/heyitsharman/Login-form",
+  },
+  {
+    img: "https://cdni.iconscout.com/illustration/premium/thumb/taking-notes-on-paper-illustration-download-in-svg-png-gif-file-formats--handwriting-mind-mapping-note-pen-and-people-pack-illustrations-6868598.png?f=webp",
+    description: "A personal To-do-List. More work to do.",
+    link: "https://github.com/heyitsharman/To-do-list",
+  },
+  {
+    img: "https://c8.alamy.com/comp/2E9BB6G/flat-cartoon-characters-family-budget-finance-management-planvector-illustration-concept-2E9BB6G.jpg",
+    description: "React Job-portal App. Currently in progress. ",
+    link: "https://github.com/heyitsharman/React-Job-portal-website",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "harmankaur0779@gmail.com",
+  // YOUR_FONE: "(555)123-4567",
+  description:
+    "Have a project in mind? I'd love to hear from you! Drop me a line or connect with me on social media  to discuss how I can help turn your ideas into reality. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_vb3syqo",
+  YOUR_TEMPLATE_ID: "template_ic4ewhe",
+  YOUR_USER_ID: "DOzaNFhBLeH0XrgQ9",
+};
+
+const socialprofils = {
+  github: "https://github.com/heyitsharman",
+  //   facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/harman%20kaur",
+  //   twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  // worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
